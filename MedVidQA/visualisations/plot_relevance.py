6069b528ae058006ee5a80c76f23620b3337cee0
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-02-17 15:32:47 +0100

    fix(visuals): minor refactor, add docs

--- MedVidQA/visualisations/plot_relevance.py
+++ MedVidQA/visualisations/plot_relevance.py
@@ -8,7 +8,7 @@ import os
 from tqdm.auto import tqdm
 
 
-def min_max_scaling(series):
+def min_max_scaling(series: pd.Series) -> pd.Series:
     return (series - series.min()) / (series.max() - series.min())
 
 
@@ -20,11 +20,26 @@ def extrapolate_scores(df: pd.DataFrame) -> pd.DataFrame:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--gold_file", default="data/raw/MedVidQA/train.json")
     parser.add_argument(
-        "--results_file", default="data/processed/transcript_passage/query_samples.csv"
+        "--gold_file",
+        default="data/raw/MedVidQA/train.json",
+        help="input json file containing start and end time of answers.",
     )
-    parser.add_argument("--model_name", default="bm25")
+    parser.add_argument(
+        "--results_file",
+        default="data/processed/transcript_passage/query_samples.csv",
+        help="results file in a csv format containing similarity score.",
+    )
+    parser.add_argument(
+        "--plots_folder",
+        default="data/plots/comparison/",
+        help="folder where plots will be saved.",
+    )
+    parser.add_argument(
+        "--model_name",
+        default="bm25",
+        help="name of the model that will be appended to the plot.",
+    )
 
     args = parser.parse_args()
 
@@ -33,6 +48,9 @@ if __name__ == "__main__":
 
     df = pd.read_csv(args.results_file)
     df = extrapolate_scores(df=df)
+
+    if not os.path.exists(args.plots_folder):
+        os.makedirs(args.plots_folder)
 
     for query_id in tqdm(df["qid"].unique().tolist()):
         tmp_df = df[df["qid"] == query_id]
@@ -56,14 +74,14 @@ if __name__ == "__main__":
 
         plt.plot(figsize=(15, 15))
 
-        sns.lineplot(data=tmp["score"], ci=0.2)
+        sns.lineplot(data=tmp["score"], ci=0.7)
         x1, y1 = [
             gold_answer["answer_start_second"],
             gold_answer["answer_end_second"],
         ], [0, 0]
         plt.plot(x1, y1, marker="x", color="red")
         plt.savefig(
-            f"data/plots/comparison/{gold_answer['sample_id']}_line_{args.model_name}.png",
+            f"{args.plots_folder}/{gold_answer['sample_id']}_line_{args.model_name}.png",
             dpi=600,
         )
         plt.cla()
@@ -71,7 +89,7 @@ if __name__ == "__main__":
         sns.displot(data=tmp_df, x="center", y="score", kind="kde", rug=True, fill=True)
         plt.plot(x1, y1, marker="x", color="red")
         plt.savefig(
-            f"data/plots/comparison/{gold_answer['sample_id']}_kde_{args.model_name}.png",
+            f"{args.plots_folder}/{gold_answer['sample_id']}_kde_{args.model_name}.png",
             dpi=600,
         )
         plt.cla()
